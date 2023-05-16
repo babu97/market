a6aaae3e15b1a904bbd7faabb3a94e805f39a8b9
babu97 <benkaimugul@gmail.com>
2023-05-16 14:26:29 +0300

    hello Hello

--- market/models.py
+++ market/models.py
@@ -17,9 +17,8 @@ class User(db.Model, UserMixin):
 
     def can_purchase(self, item_obj):
         return self.budget >= item_obj.price
-
-
-
+    def can_sell (self, item_obj):
+        return item_obj in self.items
     @property
     def prettier_budget(self):
         if len(str(self.budget)) >= 4:
@@ -53,3 +52,8 @@ class Item(db.Model):
            self.owner = user.id
            user.budget -= self.price
            db.session.commit()
+    def sell(self, user):
+        self.owner = None
+        user.budget += self.price
+        db.session.commit()
+        
